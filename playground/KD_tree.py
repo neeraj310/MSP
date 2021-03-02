9261477ba974be1d59e4021fea36b035ff491d58
nivedita Jha <niveditajha@niveditas-MBP.fritz.box>
2021-03-03 00:52:01 +0100

    2d_data_experiment

--- playground/KD_tree.py
+++ playground/KD_tree.py
@@ -1,3 +1,5 @@
+import random, cProfile,csv,sys
+import numpy as np
 
 # Makes the KD-Tree for fast lookup
 def make_kd_tree(points, dim, i=0):
@@ -61,14 +63,6 @@ def get_nearest(kd_node, point, dim, dist_func, return_distances=True, i=0, best
     return best if return_distances else best[1]
 
 
-
-"""
-Below is all the testing code
-"""
-
-import random, cProfile
-
-
 def puts(l):
     for x in l:
         print(x)
@@ -93,47 +87,60 @@ def dist_sq(a, b, dim):
 def dist_sq_dim(a, b):
     return dist_sq(a, b, dim)
 
-# set_points= [[412,273],[364,293],[32,107]]
 
-set_points= [[5,4]]
-# points = [PointContainer(rand_point(dim)) for x in range(6)]
-points = set_points
-# additional_points = [PointContainer(rand_point(dim)) for x in range(50)]
-# additional_points =[[429,195],[229,113]]
-additional_points =[[2,6],[13,3],[8,7],[3,1],[10,2]]
-#points = [rand_point(dim) for x in range(5000)]
-# test = [rand_point(dim) for x in range(100)]
-test = [[9,4]]
-result1 = []
-result2 = []
+"""
+Below is all the testing code
+"""
+if __name__ == "__main__":
+    filename=sys.argv[1]
+    # filename= "data/2d_exponential_exponential_1000000.csv"clear
+    points=[]
+    with open(filename,'r') as csvfile:
+        points_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        next(points_reader)
+        for point in points_reader:
+            points.append(list(np.float_(point[:2])))
+    
+    points=points
+    # points =points
+    test = [[3,1]]
+    result = []
+    dim=2
 
-
-def bench1():
     kd_tree = make_kd_tree(points, dim)
-    for point in additional_points:
-        add_point(kd_tree, point, dim)
-    # result1.append(tuple(get_knn(kd_tree, [0] * dim, 1, dim, dist_sq_dim)))
+    # result.append(tuple(get_knn(kd_tree, [0] * dim, 2, dim, dist_sq_dim)))
     for t in test:
-        result1.append(tuple(get_knn(kd_tree, t, 1, dim, dist_sq_dim)))
+        result.append(tuple(get_knn(kd_tree, t, 2, dim, dist_sq_dim)))
+
+    print(result)
 
 
-def bench2():
-    all_points = points + additional_points
-    result2.append(tuple(get_knn_naive(all_points, [0] * dim, 8, dist_sq_dim)))
-    for t in test:
-        result2.append(tuple(get_knn_naive(all_points, t, 8, dist_sq_dim)))
+# def bench1():
+#     kd_tree = make_kd_tree(points, dim)
+#     for point in additional_points:
+#         add_point(kd_tree, point, dim)
+#     # result1.append(tuple(get_knn(kd_tree, [0] * dim, 1, dim, dist_sq_dim)))
+#     for t in test:
+#         result1.append(tuple(get_knn(kd_tree, t, 1, dim, dist_sq_dim)))
 
-cProfile.run("bench1()")
-cProfile.run("bench2()")
 
-puts(result1[0])
-print("")
-puts(result2[0])
-print("")
+# def bench2():
+#     all_points = points + additional_points
+#     result2.append(tuple(get_knn_naive(all_points, [0] * dim, 8, dist_sq_dim)))
+#     for t in test:
+#         result2.append(tuple(get_knn_naive(all_points, t, 8, dist_sq_dim)))
 
-print("Is the result same as naive version?: {}".format(result1 == result2))
+# cProfile.run("bench1()")
+# cProfile.run("bench2()")
 
-print("")
-kd_tree = make_kd_tree(points, dim)
+# puts(result1[0])
+# print("")
+# puts(result2[0])
+# print("")
 
-print(get_nearest(kd_tree, [0] * dim, dim, dist_sq_dim))
+# print("Is the result same as naive version?: {}".format(result1 == result2))
+
+# print("")
+# kd_tree = make_kd_tree(points, dim)
+
+# print(get_nearest(kd_tree, [0] * dim, dim, dist_sq_dim))
